837de14592b3c30f0b262c06c7d72ebcf0ac28dc
Bryan Lucas da Conceição Afinovicz <bryanafinovicz706@gmail.com>
2026-03-02 10:06:58 -0300

    youtube mobile

--- youtube/script.js
+++ youtube/script.js
@@ -1,59 +1,63 @@
-  const botao = document.getElementById("botao-tema");
-  const body = document.body;
-  const youtube = document.getElementById("youtube");
-  const shorts = document.getElementById("shorts");
-  const subscribe = document.getElementById("subscribe");
-  const library = document.getElementById("library");
-  const navLateral = document.getElementById("nav-lateral");
+const body = document.body;
+const menu = document.getElementById("menu")
+const botao = document.getElementById("botao-tema");
+const youtube = document.getElementById("youtube");
+const shorts = document.getElementById("shorts");
+const subscribe = document.getElementById("subscribe");
+const library = document.getElementById("library");
+const navLateral = document.getElementById("nav-lateral");
 
-  // Persistência do tema
-  const temasalvo = localStorage.getItem("tema");
-  temaEscuro(temasalvo === "escuro");
+// Persistência do tema
+const temasalvo = localStorage.getItem("tema");
+temaEscuro(temasalvo === "escuro");
 
-  // Função para alternar entre tema claro e escuro
-  function temaEscuro(tipo) {
-    if (tipo == true) {
-      body.classList.add("escuro");
-      botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
-      youtube.src = "image/Youtube-LogoDark.png";
-      shorts.src = "image/shortsDark.png";
-      subscribe.src = "image/subscribeDark.png";
-      library.src = "image/libraryDark.png";
-    } else {
-      body.classList.remove("escuro");
-      botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
-      youtube.src = "image/Youtube-Logo.png";
-      shorts.src = "image/shorts.png";
-      subscribe.src = "image/subscribe.png";
-      library.src = "image/library.png";
-    }
+// Função para alternar entre tema claro e escuro
+function temaEscuro(tipo) {
+  if (tipo == true) {
+    body.classList.add("escuro");
+    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    youtube.src = "image/Youtube-LogoDark.png";
+    shorts.src = "image/shortsDark.png";
+    subscribe.src = "image/subscribeDark.png";
+    library.src = "image/libraryDark.png";
+  } else {
+    body.classList.remove("escuro");
+    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    youtube.src = "image/Youtube-Logo.png";
+    shorts.src = "image/shorts.png";
+    subscribe.src = "image/subscribe.png";
+    library.src = "image/library.png";
   }
+}
 
-  botao.addEventListener("click", () => {
-    const isescuro = body.classList.toggle("escuro");
-    temaEscuro(isescuro);
-    localStorage.setItem("tema", isescuro ? "escuro" : "claro");
-  });
+botao.addEventListener("click", () => {
+  const isescuro = body.classList.toggle("escuro");
+  temaEscuro(isescuro);
+  localStorage.setItem("tema", isescuro ? "escuro" : "claro");
+});
 
-  // pesquisa lateral
+// pesquisa lateral
 
-  function mudouTamanho() {
-    if (window.innerWidth >= 963 || window.innerWidth <= 620) {
-      navLateral.style.display = "block";
-    } else {
+function mudouTamanho() {
+  if (window.innerWidth >= 963 || window.innerWidth < 630) {
+    navLateral.style.display = "block";
+  } else {
+    navLateral.style.display = "none";
+  }
+}
+
+function clickMenu() {
+  if (window.innerWidth <= 963 && window.innerWidth >= 630)
+    if (navLateral.style.display == "block") {
       navLateral.style.display = "none";
+      //        navLateral.classList.remove("fadeIn");
+      //        navLateral.classList.add("fadeOut");
+    } else {
+      navLateral.style.display = "block";
+      //        navLateral.classList.remove("fadeOut");
+      //        navLateral.classList.add("fadeIn");
     }
-  }
+}
 
-  function clickMenu() {
-    if (window.innerWidth <= 963 )
-      if (navLateral.style.display == "block") {
-        navLateral.style.display = "none";
-//        navLateral.classList.remove("fadeIn");
-//        navLateral.classList.add("fadeOut");
-      } else {
-        navLateral.style.display = "block";
-//        navLateral.classList.remove("fadeOut");
-//        navLateral.classList.add("fadeIn");
-      }
-  }
+menu.addEventListener("click", clickMenu)
+window.addEventListener("resize", mudouTamanho)
